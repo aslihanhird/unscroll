34cc0835a86d0bf3a664c256b44a7cbe97fc8dab
Aslihan Hird <aslihan.hird@gmail.com>
2023-03-08 16:15:17 +0100

    processing video posts in the backend, might break things lets see

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,5 +9,5 @@ application.register("list", ListController)
 
 
 import ScrollTo from 'stimulus-scroll-to'
-const application = Application.start()
+// const application = Application.start()
 application.register('scroll-to', ScrollTo)
