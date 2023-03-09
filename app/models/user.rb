88022f893ca1f21cf348bd0fdeeb8a35ce947ba7
Damian Hallengren <djhallengren@gmail.com>
2023-03-09 16:13:21 +0100

    user creation now creates an attached favourites list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :create_favourites
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :lists, dependent: :destroy
   has_many :insta_profiles, through: :lists
-  has_many :favourite_posts
+  has_one :favourite_list
+
+  private
+
+  def create_favourites
+    FavouriteList.new(name: "Favourites", user: self)
+  end
 end
