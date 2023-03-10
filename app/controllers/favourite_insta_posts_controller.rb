d04cd8b20ef246b7d985880d9dbe1a66a58a4b8d
Aslihan Hird <aslihan.hird@gmail.com>
2023-03-10 14:54:23 +0100

    removed unnecessary lines I haad lying about

--- app/controllers/favourite_insta_posts_controller.rb
+++ app/controllers/favourite_insta_posts_controller.rb
@@ -13,13 +13,7 @@ class FavouriteInstaPostsController < ApplicationController
       new_fav_insta_post.media_url = og_insta_post.photo.key
       new_fav_insta_post.media_type = 'photo'
     end
-    p "..................................."
-    p new_fav_insta_post
-    p ".................................."
     new_fav_insta_post.save
-    p "..................................."
-    p new_fav_insta_post
-    p ".................................."
   end
 
   def destroy
@@ -34,18 +28,10 @@ class FavouriteInstaPostsController < ApplicationController
     return checked_insta_profile if checked_insta_profile != nil
 
     og_insta_profile = InstaProfile.find_by(username: og_insta_post.insta_profile.username)
-    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    p og_insta_profile
-    puts "Original profile"
-    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     new_favourite_insta_profile = FavouriteInstaProfile.new(username: og_insta_profile.username)
     new_favourite_insta_profile.profile_picture_url = og_insta_profile.photo.key
     new_favourite_insta_profile.favourite_list = FavouriteList.find_by(name: 'Favourites')
     new_favourite_insta_profile.save
-    puts "/////////////////////////////////////////////////////////////"
-    p new_favourite_insta_profile
-    puts "Saved"
-    puts "/////////////////////////////////////////////////////////////"
     new_favourite_insta_profile
   end
 end
