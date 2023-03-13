d3c7a0243abba7de7742cab64fdf69eeb1a8208d
Davy De Groote <davy.degr@gmail.com>
2023-03-13 13:04:30 +0100

    add user logic for previous & next buttons

--- app/javascript/controllers/posts_controller.js
+++ app/javascript/controllers/posts_controller.js
@@ -2,16 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="posts"
 export default class extends Controller {
-  static targets = ["post"]
+  static targets = ["post", "previous", "next"]
 
   connect() {
     // Remove d-none only from the very first post
     this.postTarget.classList.remove("d-none");
     this.postTarget.classList.add("displayed");
 
+    this.previousTarget.classList.add("d-none");
+
   }
 
   previous(event) {
+    event.preventDefault();
+
+    const targets = this.postTargets;
+    const current_post_index = this.#getIndex();
+
+    targets[current_post_index].classList.add("d-none");
+    targets[current_post_index].classList.remove("displayed");
+
+    targets[current_post_index - 1].classList.remove("d-none");
+    targets[current_post_index - 1].classList.add("displayed");
+
+    // If we are at the last post and press previous, show the next button again
+    if (current_post_index === targets.length - 1) {
+      this.nextTarget.classList.remove("d-none");
+    }
+
+    // If we are at the second post and press previous, hide the previous button
+    if (current_post_index === 1) {
+      this.previousTarget.classList.add("d-none");
+    }
+
 
   }
 
@@ -19,14 +42,31 @@ export default class extends Controller {
     event.preventDefault();
 
     const targets = this.postTargets;
-    const current_post_index = targets.findIndex(post => post.classList.contains("displayed"));
+    const current_post_index = this.#getIndex();
 
-    // current_post_index.classList.add("d-none");
     targets[current_post_index].classList.add("d-none");
     targets[current_post_index].classList.remove("displayed");
 
     targets[current_post_index + 1].classList.remove("d-none");
     targets[current_post_index + 1].classList.add("displayed");
 
+    this.previousTarget.classList.remove("d-none");
+
+    console.log("Current post:");
+    console.log(current_post_index);
+
+    console.log("Targets length:");
+    console.log(targets.length - 2);
+
+
+    if (current_post_index === targets.length - 2) {
+      this.nextTarget.classList.add("d-none");
+    }
+
+  }
+
+  #getIndex() {
+    const targets = this.postTargets;
+    return targets.findIndex(post => post.classList.contains("displayed"))
   }
 }
