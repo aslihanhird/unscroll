fe6b11a147abcd7059b34dd5fe48d61627a8e5c9
loic de deyn <loicdedeyn.a@gmail.com>
2023-03-14 15:32:25 +0100

    connected the controller with the target (the form to save)

--- app/javascript/controllers/save_controller.js
+++ app/javascript/controllers/save_controller.js
@@ -2,6 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="save"
 export default class extends Controller {
+
+  static targets = ['favourite']
+
   connect() {
+  console.log('connected')
+  }
+
+  savePost() {
+    const save = this.favouriteTarget
+    console.log(save)
   }
 }
