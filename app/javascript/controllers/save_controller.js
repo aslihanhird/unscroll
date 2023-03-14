4f63356c53cd4611274b253bc602840cc7c9c815
loic de deyn <loicdedeyn.a@gmail.com>
2023-03-14 17:35:48 +0100

    button save is working but need some improvement

--- app/javascript/controllers/save_controller.js
+++ app/javascript/controllers/save_controller.js
@@ -10,7 +10,25 @@ export default class extends Controller {
   }
 
   savePost() {
+
     const save = this.favouriteTarget
     console.log(save)
+    const bookmark = document.getElementById("bookmark")
+    console.log(bookmark)
+    console.log(bookmark.innerHTML)
+
+
+    if (bookmark.innerHTML === `<i class="fa-solid fa-bookmark"></i>`) {
+      bookmark.innerHTML = `<i class="fa-regular fa-bookmark"></i>`
+      console.log("test1")
+    } else {
+      bookmark.innerHTML = `<i class="fa-solid fa-bookmark"></i>`;
+      console.log("test2")
+    }
+    console.log(bookmark.innerHTML)
+
+
   }
+
+
 }
