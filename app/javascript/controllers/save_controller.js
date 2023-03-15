fd36da1bdc43ff24d27fbe59f32f9263c52cecdd
Davy De Groote <davy.degr@gmail.com>
2023-03-15 11:00:14 +0100

    Favourites are fully functional everywhere

--- app/javascript/controllers/save_controller.js
+++ app/javascript/controllers/save_controller.js
@@ -20,16 +20,12 @@ export default class extends Controller {
   savePost() {
     const allFavourites = this.favouriteFormTargets;
 
-    console.log(allFavourites);
-
     const allBookmarks = this.bookmarkTargets;
 
-    console.log(allBookmarks);
+    const index = allFavourites.findIndex(favourite => favourite.classList.contains("displayed"));
 
-    const index_of_displayed_save_form = allFavourites.findIndex(favourite => favourite.classList.contains("displayed"));
+    const bookmark = allBookmarks[(index < 0 ? 0 : index)];
 
-    const save = allFavourites[index_of_displayed_save_form];
-    const bookmark = allBookmarks[index_of_displayed_save_form];
 
     if (bookmark.innerHTML.trim() === `<i class="fa-solid fa-bookmark"></i>`) {
       bookmark.innerHTML = `<i class="fa-regular fa-bookmark"></i>`
